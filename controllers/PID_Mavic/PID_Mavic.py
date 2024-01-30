0cbbaf9c967aec40e5fe739bf8d4fc5ff346d1b0
sinatb <sinatb.dev@gmail.com>
2024-01-30 11:06:54 +0330

    added webots base controller for mavic

--- controllers/PID_Mavic/PID_Mavic.py
+++ controllers/PID_Mavic/PID_Mavic.py
@@ -1,94 +1,173 @@
 from controller import Robot
-from PID import PID
-
-robot = Robot()
-
-TIME_STEP = int(robot.getBasicTimeStep())
-PI = 3.1415926535897932384626433
-# Helping Functions
-def set_speed(v1,v2,v3,v4):
-    # Get the motors of the robot
-    frontLeftMotor = robot.getDevice('front left propeller')
-    frontRightMotor = robot.getDevice('front right propeller')
-    backLeftMotor = robot.getDevice('rear left propeller')
-    backRightMotor = robot.getDevice('rear right propeller')
-    # Set the speed of the motors
-    frontLeftMotor.setVelocity(v1)
-    frontRightMotor.setVelocity(v2)
-    backLeftMotor.setVelocity(v3)
-    backRightMotor.setVelocity(v4)
-    return    
-
-# Robot Setup
-
-# Motors Setup
-frontLeftMotor = robot.getDevice('front left propeller')
-frontRightMotor = robot.getDevice('front right propeller')
-backLeftMotor = robot.getDevice('rear left propeller')
-backRightMotor = robot.getDevice('rear right propeller')
-
-frontLeftMotor.setPosition(float('inf'))
-frontRightMotor.setPosition(float('inf'))
-backLeftMotor.setPosition(float('inf'))
-backRightMotor.setPosition(float('inf'))
-
-set_speed(160,160,160,160)
-
-# Setting LED
-front_left_led = robot.getDevice("front left led")
-front_right_led = robot.getDevice("front right led")
-# Setting gps
-gps = robot.getDevice("gps")
-gps.enable(TIME_STEP)
-# Setting imu
-imu = robot.getDevice("inertial unit")
-imu.enable(TIME_STEP)
-# Setting compass
-compass = robot.getDevice("compass")
-compass.enable(TIME_STEP)
-# Setting gyro
-gyro = robot.getDevice("gyro")
-gyro.enable(TIME_STEP)
-
-yaw_setpoint=-1
-
-pitchPID = PID(1,0.1,5,goal=0.0)
-rollPID = PID(1,0.1,5,goal=0.0)
-throttlePID = PID(10,0.1,5,goal=1.0)
-yawPID = PID(1,0.1,5,goal=float(yaw_setpoint))
-
-targetX, targetY, target_altitude = 0.0, 0.0, 1.0
-
-while (robot.step(TIME_STEP) != -1):
-
-	led_state = int(robot.getTime()) % 2
-	front_left_led.set(led_state)
-	front_right_led.set(int(not(led_state)))
-
-	roll = imu.getRollPitchYaw()[0] + PI / 2.0
-	pitch = imu.getRollPitchYaw()[1]
-	yaw = compass.getValues()[0]
-	roll_acceleration = gyro.getValues()[0]
-	pitch_acceleration = gyro.getValues()[1]
-	
-	xGPS = gps.getValues()[2]
-	yGPS = gps.getValues()[0]
-	zGPS = gps.getValues()[1]
-
-	vertical_input = throttlePID(zGPS)
-	yaw_input = yawPID(yaw)
-
-	rollPID.goal = targetX
-	pitchPID.goal = targetY
-	
-	roll_input = 10 * roll + roll_acceleration + rollPID(xGPS)
-	pitch_input = 10 * pitch - pitch_acceleration + pitchPID(-yGPS)
-
-	front_left_motor_input = 68.5 + vertical_input - roll_input - pitch_input + yaw_input
-	front_right_motor_input = 68.5 + vertical_input + roll_input - pitch_input - yaw_input
-	rear_left_motor_input = 68.5 + vertical_input - roll_input + pitch_input - yaw_input
-	rear_right_motor_input = 68.5 + vertical_input + roll_input + pitch_input + yaw_input
-
-	set_speed(front_left_motor_input, -front_right_motor_input, -rear_left_motor_input, rear_right_motor_input)
+import sys
+try:
+    import numpy as np
+except ImportError:
+    sys.exit("Warning: 'numpy' module not found.")
 
 
+def clamp(value, value_min, value_max):
+    return min(max(value, value_min), value_max)
+
+
+class Mavic (Robot):
+    # Constants, empirically found.
+    K_VERTICAL_THRUST = 68.5  # with this thrust, the drone lifts.
+    # Vertical offset where the robot actually targets to stabilize itself.
+    K_VERTICAL_OFFSET = 0.6
+    K_VERTICAL_P = 3.0      # P constant of the vertical PID.
+    K_ROLL_P = 50.0           # P constant of the roll PID.
+    K_PITCH_P = 30.0          # P constant of the pitch PID.
+
+    MAX_YAW_DISTURBANCE = 0.4
+    MAX_PITCH_DISTURBANCE = -1
+    # Precision between the target position and the robot position in meters
+    target_precision = 0.4
+
+    def __init__(self):
+        Robot.__init__(self)
+
+        self.time_step = int(self.getBasicTimeStep())
+
+        # Get and enable devices.
+        self.camera = self.getDevice("camera")
+        self.camera.enable(self.time_step)
+        self.imu = self.getDevice("inertial unit")
+        self.imu.enable(self.time_step)
+        self.gps = self.getDevice("gps")
+        self.gps.enable(self.time_step)
+        self.gyro = self.getDevice("gyro")
+        self.gyro.enable(self.time_step)
+        
+
+        self.front_left_motor = self.getDevice("front left propeller")
+        self.front_right_motor = self.getDevice("front right propeller")
+        self.rear_left_motor = self.getDevice("rear left propeller")
+        self.rear_right_motor = self.getDevice("rear right propeller")
+        self.camera_pitch_motor = self.getDevice("camera pitch")
+        self.camera_pitch_motor.setPosition(0.7)
+        motors = [self.front_left_motor, self.front_right_motor,
+                  self.rear_left_motor, self.rear_right_motor]
+        for motor in motors:
+            motor.setPosition(float('inf'))
+            motor.setVelocity(1)
+
+        self.current_pose = 6 * [0]  # X, Y, Z, yaw, pitch, roll
+        self.probs = [-1.0] * 5
+        self.target_position = [0, 0, 0]
+        self.target_index = 0
+        self.target_altitude = 0
+    
+    def CNN(self,photo):
+        pass
+    def set_position(self, pos):
+        self.current_pose = pos
+        
+    def check_position(self,target_index,position):
+        #retruns bool
+        if target_index == 0:
+            return (position[0] > -5.4 and position[0] < -5.0) and (position[1] > 3.4 and position[1] < 3.8)
+        elif target_index == 1:
+            return (position[0] > -3.7 and position[0] < -3.3) and (position[1] > -2.8 and position[1] < -2.4)
+        elif target_index == 2:
+            return (position[0] > 2.6 and position[0] < 2.8) and (position[1] > -2.8 and position[1] < -2.4)
+        elif target_index == 3:
+            return (position[0] > 5.5 and position[0] < 5.8) and (position[1] > 0.1 and position[1] < 0.5)
+        elif target_index == 4:
+            return (position[0] > 1.5 and position[0] < 1.7) and (position[1] > 4.6 and position[1] < 4.8)
+            
+    def move_to_target(self, waypoints, verbose_movement=False, verbose_target=False):
+        if self.target_position[0:2] == [0, 0]:  # Initialization
+            self.target_position[0:2] = waypoints[0]
+            if verbose_target:
+                print("First target: ", self.target_position[0:2])
+
+        if self.check_position(self.target_index,self.current_pose[0:2]):
+            name = "photo"+str(self.target_index)+".jpg"
+            self.camera.saveImage(name,100)
+            self.probs[self.target_index] = self.CNN(name)
+
+        # if the robot is at the position with a precision of target_precision
+        if all([abs(x1 - x2) < self.target_precision for (x1, x2) in zip(self.target_position, self.current_pose[0:2])]):
+            
+            self.target_index += 1
+            if self.target_index > len(waypoints) - 1:
+                self.target_index = 0
+            self.target_position[0:2] = waypoints[self.target_index]
+            if verbose_target:
+                print("Target reached! New target: ",
+                      self.target_position[0:2])
+
+        # This will be in ]-pi;pi]
+        self.target_position[2] = np.arctan2(
+            self.target_position[1] - self.current_pose[1], self.target_position[0] - self.current_pose[0])
+        # This is now in ]-2pi;2pi[
+        angle_left = self.target_position[2] - self.current_pose[5]
+        # Normalize turn angle to ]-pi;pi]
+        angle_left = (angle_left + 2 * np.pi) % (2 * np.pi)
+        if (angle_left > np.pi):
+            angle_left -= 2 * np.pi
+
+        # Turn the robot to the left or to the right according the value and the sign of angle_left
+        yaw_disturbance = self.MAX_YAW_DISTURBANCE * angle_left / (2 * np.pi)
+        # non proportional and decreasing function
+        pitch_disturbance = clamp(
+            np.log10(abs(angle_left)), self.MAX_PITCH_DISTURBANCE, 0.1)
+
+        if verbose_movement:
+            distance_left = np.sqrt(((self.target_position[0] - self.current_pose[0]) ** 2) + (
+                (self.target_position[1] - self.current_pose[1]) ** 2))
+            print("remaning angle: {:.4f}, remaning distance: {:.4f}".format(
+                angle_left, distance_left))
+        return yaw_disturbance, pitch_disturbance
+
+    def run(self):
+        t1 = self.getTime()
+
+        
+        roll_disturbance = 0
+        pitch_disturbance = 0
+        yaw_disturbance = 0
+
+        # Specify the patrol coordinates
+        waypoints = [[-5, 4], [-3, -2], [3, -3.5], [5, 0],[2,5]]
+        # target altitude of the robot in meters
+        self.target_altitude = 3
+
+        while self.step(self.time_step) != -1:
+
+            # Read sensors
+            roll, pitch, yaw = self.imu.getRollPitchYaw()
+            x_pos, y_pos, altitude = self.gps.getValues()
+            roll_acceleration, pitch_acceleration, _ = self.gyro.getValues()
+            self.set_position([x_pos, y_pos, altitude, roll, pitch, yaw])
+
+            if altitude > self.target_altitude - 1:
+                # as soon as it reach the target altitude, compute the disturbances to go to the given waypoints.
+                if self.getTime() - t1 > 0.1:
+                    yaw_disturbance, pitch_disturbance = self.move_to_target(
+                        waypoints,
+                        verbose_target = True)
+                    t1 = self.getTime()
+
+            roll_input = self.K_ROLL_P * clamp(roll, -1, 1) + roll_acceleration + roll_disturbance
+            pitch_input = self.K_PITCH_P * clamp(pitch, -1, 1) + pitch_acceleration + pitch_disturbance
+            yaw_input = yaw_disturbance
+            clamped_difference_altitude = clamp(self.target_altitude - altitude + self.K_VERTICAL_OFFSET, -1, 1)
+            vertical_input = self.K_VERTICAL_P * pow(clamped_difference_altitude, 3.0)
+
+            front_left_motor_input = self.K_VERTICAL_THRUST + vertical_input - yaw_input + pitch_input - roll_input
+            front_right_motor_input = self.K_VERTICAL_THRUST + vertical_input + yaw_input + pitch_input + roll_input
+            rear_left_motor_input = self.K_VERTICAL_THRUST + vertical_input + yaw_input - pitch_input - roll_input
+            rear_right_motor_input = self.K_VERTICAL_THRUST + vertical_input - yaw_input - pitch_input + roll_input
+
+            self.front_left_motor.setVelocity(front_left_motor_input)
+            self.front_right_motor.setVelocity(-front_right_motor_input)
+            self.rear_left_motor.setVelocity(-rear_left_motor_input)
+            self.rear_right_motor.setVelocity(rear_right_motor_input)
+
+
+# To use this controller, the basicTimeStep should be set to 8 and the defaultDamping
+# with a linear and angular damping both of 0.5
+robot = Mavic()
+robot.run()
